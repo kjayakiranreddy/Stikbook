cd3cff107f3f2c1982eec0522171ab130dff08ed
mkotha <mkotha@B31QFT2>
2020-08-11 11:35:06 -0400

    Backend for Signup and Login has been added

---
+++ stikbook-backend/index.js
@@ -0,0 +1,36 @@
+const debug = require('debug')('app:startup');
+const helmet = require('helmet'); // Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
+const config  = require('config'); 
+const mongoose = require('mongoose');
+const morgan  = require('morgan');//HTTP request logger middleware for node.js
+const Joi     = require('joi'); // The most powerful schema description language and data validator for JavaScript.
+const logger  = require('./middleware/logger');
+const courses = require('./routes/courses');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
+const home    = require('./routes/home');
+const authenticate = require('./authentication');
+const express = require('express');
+const app = express();
+
+
+mongoose.connect('mongodb://localhost/stikbook')
+    .then( ()=>{ console.log('Connected to MongoDB...') })
+    .catch(err => console.error('Could not connected to MongoDB...') );
+
+app.set('view engine','pug');
+app.set('views','./views');
+
+app.use(express.json()); 
+app.use(express.urlencoded({extended : true}));
+app.use(express.static('public'));
+app.use(helmet());
+app.use('/api/courses',courses);
+app.use('/api/users',users);
+app.use('/api/auth',auth);
+app.use('/',home);
+
+if(app.get('env') === 'development') { app.use(morgan('tiny'));};
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
