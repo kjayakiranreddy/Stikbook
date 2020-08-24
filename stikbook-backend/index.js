5a9b8790053392b76e5dbe0a868430600a7b0a44
KMuraliReddy <kothamurali08719@gmail.com>
2020-08-24 19:31:40 -0400

    backend

--- stikbook-backend/index.js
+++ stikbook-backend/index.js
@@ -11,46 +11,10 @@ const express = require('express');
 const app = express();
 
 
-mongoose.connect('mongodb://localhost/stikbook')
+mongoose.connect('mongodb://localhost/stikbook',{useFindAndModify : false})
     .then( ()=>{ console.log('Connected to MongoDB...') })
     .catch(err => console.error('Could not connected to MongoDB...') );
 
-//random otp generator
-var randomstring = require("randomstring");
-
-const secretToken = randomstring.generate({
-  length:6,
-  charset: 'numeric'
-});
-//nodemailer
-    var nodemailer = require('nodemailer');
-    var transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'sindhu139@gmail.com',
-        pass: 'Chethan@143'
-      }
-    });
-    
-    var mailOptions = {
-      from: 'sindhu139@gmail.com',
-      to: 'jaykiranreddy@gmail.com,p.sindhura515@gmail.com',
-      subject: 'Welcome to Stikbook',
-      text: ``,
-      html: `<h2>Dear Applicant,</h2><h4>Thanks for Resgistering with STIKbook.</h4>
-      <p>Your one time verification code is: </p>`
-      +JSON.parse(JSON.stringify(secretToken))+`<p> and is valid for 10 minutes. (Generated at </p>`+new Date()+`)
-      <p>*******************************</p><br>
-      <p>This is an auto generated mail. Do not reply to this mail.</p>`,
-    };
-    
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error); 
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
 
 app.set('view engine','pug');
 app.set('views','./views');
